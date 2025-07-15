af4fba7a8493c5cdfe552814b2a34092e6b869be
Ali Neameh <alineameh76@gmail.com>
2025-07-15 17:26:45 +0300

    using a mapper library

--- subscription-service/src/main/java/com/example/subscription_service/service/PlanService.java
+++ subscription-service/src/main/java/com/example/subscription_service/service/PlanService.java
@@ -6,7 +6,6 @@ import com.example.subscription_service.dto.PlanResponseDto;
 import com.example.subscription_service.mapper.PlanMapper;
 import com.example.subscription_service.model.Plan;
 import com.example.subscription_service.repository.PlanRepository;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +15,12 @@ public class PlanService {
 
     private final PlanRepository planRepository;
 
+    private final PlanMapper planMapper;
+
     public PlanResponseDto createPlan(PlanRequestDto planRequestDto) {
-        Plan plan= planRepository.save(PlanMapper.toModel(planRequestDto));
-        return PlanMapper.toDto(plan);
+        Plan plan = planRepository.save(planMapper.toModel(planRequestDto));
+        return planMapper.toDto(plan);
     }
-
-
 
 
 }
